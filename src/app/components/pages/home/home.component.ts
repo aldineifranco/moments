a1cb52a1d7561ee534c17b01d2e8a160353b0f59
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-08-01 08:19:40 -0300

    input busca fotos

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -11,9 +11,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
 
-  allMoments: Moment[] = []
-  moments: Moment[] = []
-  baseApiUrl = environment.baseApiUrl
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
+
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) { }
 
@@ -30,6 +33,15 @@ export class HomeComponent implements OnInit {
       this.allMoments = data
       this.moments = data
     });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
 }
 
