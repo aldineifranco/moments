5789dd539aecf4f71b1833a33d7b46d1be30bada
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-08-01 11:21:37 -0300

    html e css página momentos

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moments';
 import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -10,6 +13,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentService: MomentService, private route: ActivatedRoute) { }
 
